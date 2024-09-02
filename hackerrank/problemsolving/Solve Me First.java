b24336ba49ff2221ec3ec91249694b6409d0ca9f
Dhatri <110086564+dha3-m@users.noreply.github.com>
2024-09-02 21:33:57 +0530

    Solve Me First.

---
+++ hackerrank/problemsolving/Solve Me First.java
@@ -0,0 +1,42 @@
+/** Complete the function solveMeFirst to compute the sum of two integers.
+
+Example
+a = 7
+b = 3
+Return 10.
+
+Function Description
+
+Complete the solveMeFirst function in the editor below.
+
+solveMeFirst has the following parameters:
+
+int a: the first value
+int b: the second value
+Returns
+- int: the sum of a and b **/
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+    static int solveMeFirst(int a, int b) {
+      	return a + b;
+
+   }
+
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+   }
+}
